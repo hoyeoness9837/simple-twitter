75211b63ae3fc78f93993a2132f65555cc269b8d
Ho yeon KIM <hoyeonkim@Hos-MacBook-Pro.local>
2020-09-18 00:16:59 -0700

    simple input and button for password and email.

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <div>hello world!</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          value={email}
+          onChange={onChange}
+          type="text"
+          placeholder="Email"
+          required
+        />
+        <input
+          name="password"
+          value={password}
+          onChange={onChange}
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <input type="submit" placeholder="Log In" />
+      </form>
+      <div>
+        <button> Continue with Google Account</button>
+        <button> Continue with GitHub Account</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
