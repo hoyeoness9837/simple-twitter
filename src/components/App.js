65fc4a65af43a3a112a73cdaa0e64e8cab82e34b
Ho yeon KIM <hoyeonkim@Hos-MacBook-Pro.local>
2020-09-18 03:33:31 -0700

    using onAuthStateChanged to automatically check if user is logged in, then hide AppRouter.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import AppRouter from "components/Router";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { authService } from "fbase";
 
 function App() {
-  console.log(authService.currentUser)
-  const [isLoogedIn, setIsLoggedIn] = useState(false);
+  const [init, setInit] = useState(false);
+  const [isLoogedIn, setIsLoggedIn] = useState(authService.currentUser);
+  
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
   return (
     <>
-      <AppRouter isLoogedIn={isLoogedIn} />
+      {init ? <AppRouter isLoogedIn={isLoogedIn} /> : "Fire Base is Initializing..."}
       <footer>footer</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,12 @@
+import { authService } from "fbase";
+import { auth } from "firebase";
 import React, { useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("")
   const onChange = (e) => {
     const {
       target: { name, value },
@@ -13,9 +17,24 @@ const Auth = () => {
       setPassword(value);
     }
   };
-  const onSubmit = (e) => {
+
+  const onSubmit = async (e) => {
     e.preventDefault();
+    try  {
+    let data;
+    if  (newAccount) {
+      data = await authService.createUserWithEmailAndPassword(email,password);
+    } else {
+      data = await authService.signInWithEmailAndPassword(email,password);
+    } 
+    console.log(data)
+  } catch (error){
+      setError(error.message);
+    }
   };
+  
+  const toggleAccount = () => setNewAccount((prev)=>!prev)
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -35,8 +54,10 @@ const Auth = () => {
           placeholder="Password"
           required
         />
-        <input type="submit" placeholder="Log In" />
+        <input type="submit" value={newAccount ? "Create Account" : "Sign In"}/>
+      {error}
       </form>
+  <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
       <div>
         <button> Continue with Google Account</button>
         <button> Continue with GitHub Account</button>
