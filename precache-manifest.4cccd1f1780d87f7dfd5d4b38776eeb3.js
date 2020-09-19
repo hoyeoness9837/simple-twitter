0303865c16651bc93b8fafae380aa1f927a4c0f7
Ho yeon KIM <hoyeonkim@Hos-MacBook-Pro.local>
2020-09-18 23:32:14 -0700

    Updates

---
+++ precache-manifest.4cccd1f1780d87f7dfd5d4b38776eeb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62dc798ed6011951a83a9972b916f819",
+    "url": "/simple-twitter/index.html"
+  },
+  {
+    "revision": "6e9f3e7f79a8a89d62e8",
+    "url": "/simple-twitter/static/css/main.9be6c352.chunk.css"
+  },
+  {
+    "revision": "bb3726bc940cc66b2dec",
+    "url": "/simple-twitter/static/js/2.c6f9be98.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/simple-twitter/static/js/2.c6f9be98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9f3e7f79a8a89d62e8",
+    "url": "/simple-twitter/static/js/main.06c864a2.chunk.js"
+  },
+  {
+    "revision": "6d2e13d28d3ab2c4e0d6",
+    "url": "/simple-twitter/static/js/runtime-main.3c27efc4.js"
+  }
+]);
